ae294925e668b0b0054f0db3436ab03049c07d8c
nerett <pt-mvb@inbox.lv>
2025-10-03 23:55:21 +0300

    added comment to HW1.3

--- 1/1.3_fixed.cc
+++ 1/1.3_fixed.cc
@@ -2,10 +2,12 @@
 
 int foo(int *a, int base, int off) {
     if (off > 0) {
+        // Check for 'base + off > INT_MAX' overflow
         if (base > std::numeric_limits<int>::max() - off)
             return 42;
     }
     else if (off < 0) {
+        // Check for 'base + off < INT_MIN' overflow
         if (base < std::numeric_limits<int>::min() - off)
             return 42;
     }

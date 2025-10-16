51d72d9daf64d367feb0e0ee8eb4c95e35a762e7
nerett <pt-mvb@inbox.lv>
2025-10-16 23:22:00 +0300

    added 2x2 matrix implementation

---
+++ 3.1/src/matrix2x2.hh
@@ -0,0 +1,48 @@
+#pragma once
+
+#include <cstddef>
+#include <stdexcept>
+
+
+struct Matrix2x2 {
+    int n[2][2];
+
+    bool operator==(const Matrix2x2& m) const {
+        for (int i = 0; i < 2; ++i)
+            for (int j = 0; j < 2; ++j)
+                if (n[i][j] != m.n[i][j])
+                    return false;
+        return true;
+    }
+
+    Matrix2x2& operator*=(const Matrix2x2& m) {
+        Matrix2x2 r;
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                int acc = 0;
+                for (int k = 0; k < 2; k++)
+                    acc += n[i][k] * m.n[k][j];
+                r.n[i][j] = acc;
+            }
+        }
+        *this = r;
+        return *this;
+    }
+
+    explicit Matrix2x2(size_t n_size) {
+        if (n_size != 2)
+            throw std::invalid_argument("Matrix2x2 can only be initialized with size 2.");
+        n[0][0] = 1; n[0][1] = 0;
+        n[1][0] = 0; n[1][1] = 1;
+    }
+
+    Matrix2x2() = default;
+
+    static constexpr size_t rows = 2;
+    static constexpr size_t cols = 2;
+};
+
+inline Matrix2x2 operator*(Matrix2x2 lhs, const Matrix2x2& rhs) {
+    lhs *= rhs;
+    return lhs;
+}
